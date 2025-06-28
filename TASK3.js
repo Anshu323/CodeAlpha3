186bd6d8b26bd6cafb02790d2648b3a9539c7e91
Anshu Patel <anshupatel54288@gmail.com>
2025-06-28 21:59:40 +0530

    Add files via upload

---
+++ TASK3.js
@@ -0,0 +1,8 @@
+// Smooth scroll animation (optional improvement)
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const section = document.querySelector(this.getAttribute('href'));
+    section.scrollIntoView({ behavior: 'smooth' });
+  });
+});
